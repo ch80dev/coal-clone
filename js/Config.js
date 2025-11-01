d4edaf64b10ed702762bf72dcb1876477ecab714
ch_80 <ch80dev@gmail.com>
2025-10-31 21:57:37 -0700

    testing lowered ore value to make the game harder

--- js/Config.js
+++ js/Config.js
@@ -16,7 +16,7 @@ class Config {
 	static ore_percents ={ coal: .1, iron: .05, gold: .01 };
 	static ore_grow_max = { coal: 2, iron: 4, gold: 10 };
 	static ore_grow_target = { coal: 1, iron: 1, gold: 1 }
-	static ore_values = { coal: 10, dirt: 0, iron: 25, gold: 100, stone: 0};
+	static ore_values = { coal: 5, dirt: 0, iron: 10, gold: 100, stone: 0};
 	
 	static start_x = 0;
 	static start_y = 2;
